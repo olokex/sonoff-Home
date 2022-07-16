cd7a09280ac768aaba56d8aba2ff1e3242e2751e
olok <olokex@gmail.com>
2022-07-16 22:06:11 +0200

    last functional almost nonprototype, done classes for kvm switch and sonoff switch

--- helloWordlTemplate.py
+++ helloWordlTemplate.py
@@ -1,73 +1,119 @@
 from flask import Flask, render_template, request
-import datetime
 import requests
+import json
+import sys
+sys.path.append('..')
+import switcher
+
+class SonOffSwitch:
+   def __init__(self, device_id, ip_address, value, name):
+      self.device_id = device_id
+      self.ip_address = ip_address
+      self.value = value
+      self.name = name
+      self.check_status()
+
+   def _send_request(self, message=None, address=None):
+      msg = {
+         "deviceid": self.device_id,
+         "data": { 
+            "switch": self.status
+         }
+      }
+      
+      response = requests.post("http://{}:8081/zeroconf/switch".format(self.ip_address), json=msg)
+      print(response)
+
+   def check_status(self):
+      msg = {
+         "deviceid": self.device_id,
+         "data": { }
+      }
+   
+      response = requests.post("http://{}:8081/zeroconf/info".format(self.ip_address), json=msg)
+      response = json.loads(response.content)
+
+      if response["data"]["switch"] == "off":
+         self._off()
+      else:
+         self._on()
+
+   def _on(self):
+      self.status = "on"
+      self.style = "btn-on"
+
+   def _off(self):
+      self.status = "off"
+      self.style = "btn-off"
+
+   def switch_status(self):
+      if self.status == "off":
+         self._on()
+      else:
+         self._off()
+      self._send_request()
+
+
+class KVMSwitch:
+   def __init__(self, value, name):
+      self.value = value
+      self.name = name
+      self._check_status()
+
+   def switch_status(self):
+      self._check_status()
+      switcher.switch_status()      
+
+   def _check_status(self):
+      if self.value == "Work":
+         self._home()
+      else:
+         self._work()
+
+   def _work(self):
+      self.value = "Work"
+      self.style = "btn-off"
+
+   def _home(self):
+      self.value = "Home"
+      self.style = "btn-on"
 
 devices = {
-   "Lamp": {
-      "deviceid": "100164af39",
-      "data": { 
-        "switch": "off"
-      },
-      "style": "btn-off"
-   },
-   "Work": {
-      "deviceid": "100164af39",
-      "data": { 
-        "switch": "off"
-      },
-      "style": "btn-off",
-   },
-   "Fan": {
-      "deviceid": "100164af39",
-      "data": { 
-        "switch": "off"
-      },
-      "style": "btn-off"
-   }
+   "Lamp": SonOffSwitch(
+      device_id="100164af39",
+      ip_address="192.168.0.127",
+      value="Lamp",
+      name="Lamp"
+   ),
+
+   "Work": KVMSwitch(
+      value="Home",
+      name="Work"
+   ),
+
 }
 
-def send_request(data):
-   status = data['data']['switch']
-
-   if data['data']['switch'] == 'off':
-      data['data']['switch'] = 'on'
-      data['style'] = 'btn-on'
-   else:
-      data['data']['switch'] = 'off'
-      data['style'] = 'btn-off'
-
-   print(data)
-   # req = requests.post("http://192.168.0.127:8081/zeroconf/switch", json=data)
-   # print(req)
 
 app = Flask(__name__)
 
+def process_post(site):
+   if request.method == 'POST':
+      device = request.form["device"]
+      print(device)
+      if device in devices:
+         devices[device].switch_status()
+
+   return render_template(site, devices=devices)
+
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
-   templateData = {
-      'time': "Hello Simi",
-      'devices': devices
-   }
+   return process_post("index.html")
 
-   if request.method == 'POST':
-      submited = request.form.to_dict(flat=False)
-      key = submited.popitem()[0]
-      send_request(devices[key])
-
-   return render_template("index.html", **templateData)
 
 @app.route("/mamka/", methods=['GET', 'POST'])
 def mamka():
-   templateData = {
-      'devices': devices
-   }
-
-   if request.method == 'POST':
-      submited = request.form.to_dict(flat=False)
-      key = submited.popitem()[0]
-      send_request(devices[key])
-
-   return render_template("mamka.html", **templateData)
+   return process_post("mamka.html")
 
 
 if __name__ == "__main__":
